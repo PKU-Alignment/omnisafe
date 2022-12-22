dbec13e7f379ddc5fdc91350980b402d0f619e7f
Gaiejj <524339208@qq.com>
2022-12-22 19:18:47 +0800

    refactor: reformat the comments

--- omnisafe/wrappers/early_terminated_wrapper.py
+++ omnisafe/wrappers/early_terminated_wrapper.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Early terminated wrappe"""
+"""Early terminated wrapper."""
 
 import torch
 
--- omnisafe/wrappers/on_policy_wrapper.py
+++ omnisafe/wrappers/on_policy_wrapper.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Enviroment wrapper for on-policy algorithms."""
+"""Environment wrapper for on-policy algorithms."""
 
 import collections
 from copy import deepcopy
