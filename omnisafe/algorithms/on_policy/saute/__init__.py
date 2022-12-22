d4b3317e3a9405e688cbbce52d528ecb7c339060
Gaiejj <524339208@qq.com>
2022-12-22 17:21:37 +0800

    feat: add __init__.py

--- omnisafe/algorithms/on_policy/saute/__init__.py
+++ omnisafe/algorithms/on_policy/saute/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""On-policy algorithms."""
+"""Saute algorithms."""
