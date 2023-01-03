91bfb5c9b1c532ecc710943fbfc60d3f539a57c5
Borong Zhang <maydayqq@yeah.net>
2023-01-03 20:04:17 +0800

    wip

--- envs/safety-gymnasium/tests/test_vision_env.py
+++ envs/safety-gymnasium/tests/test_vision_env.py
@@ -20,7 +20,7 @@ import helpers
 
 
 @helpers.parametrize(
-    agent_id=['Point', 'Car'], env_id=['Goal', 'Push', 'Button'], level=['0', '1', '2']
+    agent_id=['Point', 'Car', 'Racecar'], env_id=['Goal', 'Push', 'Button'], level=['0', '1', '2']
 )
 # pylint: disable-next=too-many-locals
 def test_vision_env(agent_id, env_id, level):
@@ -48,3 +48,38 @@ def test_vision_env(agent_id, env_id, level):
 
         ep_ret += reward
         ep_cost += cost
+
+
+@helpers.parametrize(
+    agent_id=['Point', 'Car', 'Racecar'],
+    env_id=['Run'],
+    level=['0'],
+)
+# pylint: disable-next=too-many-locals
+def test_new_env(agent_id, env_id, level, render_mode):
+    """Test env."""
+    env_name = 'Safety' + agent_id + env_id + level + 'Vision' + '-v0'
+    env = safety_gymnasium.make(env_name, render_mode=render_mode)
+    obs, _ = env.reset()
+    # Use below to specify seed.
+    # obs, _ = env.reset(seed=0)
+    terminated, truncated = False, False
+    ep_ret, ep_cost = 0, 0
+    for step in range(1000):  # pylint: disable=unused-variable
+        if terminated or truncated:
+            print(f'Episode Return: {ep_ret} \t Episode Cost: {ep_cost}')
+            ep_ret, ep_cost = 0, 0
+            obs, _ = env.reset()
+        assert env.observation_space.contains(obs)
+        act = env.action_space.sample()
+        assert env.action_space.contains(act)
+
+        # Use the environment's built_in max_episode_steps
+        if hasattr(env, '_max_episode_steps'):  # pylint: disable=protected-access
+            max_ep_len = env._max_episode_steps  # pylint: disable=unused-variable,protected-access
+        # pylint: disable-next=unused-variable
+        obs, reward, cost, terminated, truncated, info = env.step(act)
+        ep_ret += reward
+        ep_cost += cost
+
+        env.render()
