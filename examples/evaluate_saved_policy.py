f0208776666e2d32fb5e764cdaecc5b69ed398f6
Gaiejj <524339208@qq.com>
2023-01-13 19:29:29 +0800

    docs: update docstring

--- examples/evaluate_saved_policy.py
+++ examples/evaluate_saved_policy.py
@@ -28,4 +28,4 @@ if __name__ == '__main__':
     for item in os.scandir(os.path.join(LOG_DIR, 'torch_save')):
         if item.is_file() and item.name.split('.')[-1] == 'pt':
             evaluator.load_saved_model(save_dir=LOG_DIR, model_name=item.name)
-            evaluator.render(num_episode=10, width=256, height=256)
+            evaluator.render(num_episode=10, camera_name='track', width=256, height=256)
