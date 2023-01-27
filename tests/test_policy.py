71ed1cd3ecaebe006c08935d3281471c8315f577
Gaiejj <524339208@qq.com>
2023-01-27 13:36:17 +0800

    fix: fix test_policy.py

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -43,24 +43,7 @@ def test_off_policy(off_policy_algo):
         'epochs': 1,
         'steps_per_epoch': 1000,
         'update_after': 900,
+        'update_every': 50,
     }
     agent = omnisafe.Agent(off_policy_algo, env_id, custom_cfgs=custom_cfgs, parallel=1)
     agent.learn()
-
-
-def test_evaluate_saved_policy():
-    """Test render policy."""
-    DIR = os.path.join(os.path.dirname(__file__), 'runs')
-    evaluator = omnisafe.Evaluator()
-    for env in os.scandir(DIR):
-        env_path = os.path.join(DIR, env)
-        for algo in os.scandir(env_path):
-            print(algo)
-            algo_path = os.path.join(env_path, algo)
-            for exp in os.scandir(algo_path):
-                exp_path = os.path.join(algo_path, exp)
-                for item in os.scandir(os.path.join(exp_path, 'torch_save')):
-                    if item.is_file() and item.name.split('.')[-1] == 'pt':
-                        evaluator.load_saved_model(save_dir=exp_path, model_name=item.name)
-                        evaluator.evaluate(num_episodes=1)
-                        evaluator.render(num_episode=1, camera_name='track', width=256, height=256)
