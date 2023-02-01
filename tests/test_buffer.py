f0b2324396b58d379e5df5fa52c4caca6928c4b1
Jiayi Zhou <108712610+Gaiejj@users.noreply.github.com>
2023-02-01 23:45:15 +0800

    chore: update ci (#90)

---
+++ tests/test_buffer.py
@@ -0,0 +1,240 @@
+# Copyright 2022 OmniSafe Team. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Test Buffers"""
+
+import torch
+
+import helpers
+from omnisafe.common.buffer import Buffer
+from omnisafe.common.vector_buffer import VectorBuffer
+
+
+@helpers.parametrize(
+    obs_dim=[1],
+    act_dim=[1],
+    size=[100],
+    gamma=[0.9],
+    lam=[0.9],
+    adv_estimation_method=['gae', 'vtrace', 'gae-rtg', 'plain'],
+    standardized_rew_adv=[True],
+    standardized_cost_adv=[True],
+    lam_c=[0.9],
+    penalty_param=[0.0],
+    device=['cpu'],
+    num_envs=[1],
+)
+def test_vector_buffer(
+    obs_dim: int,
+    act_dim: int,
+    size: int,
+    gamma: float,
+    lam: float,
+    adv_estimation_method: str,
+    standardized_rew_adv: bool,
+    standardized_cost_adv: bool,
+    lam_c: float,
+    penalty_param: float,
+    device: str,
+    num_envs: int,
+) -> None:
+    """Test the VectorBuffer class."""
+    vector_buffer = VectorBuffer(
+        obs_dim=obs_dim,
+        act_dim=act_dim,
+        size=size,
+        gamma=gamma,
+        lam=lam,
+        adv_estimation_method=adv_estimation_method,
+        standardized_rew_adv=standardized_rew_adv,
+        standardized_cost_adv=standardized_cost_adv,
+        lam_c=lam_c,
+        penalty_param=penalty_param,
+        device=device,
+        num_envs=num_envs,
+    )
+    # checking the initialized value
+    assert (
+        vector_buffer.num_buffers == num_envs
+    ), f'vector_buffer.num_buffers is {vector_buffer.num_buffers}'
+    assert (
+        vector_buffer.standardized_cost_adv == standardized_cost_adv
+    ), f'vector_buffer.standardized_cost_adv is {vector_buffer.standardized_cost_adv}'
+    assert (
+        vector_buffer.standardized_rew_adv == standardized_rew_adv
+    ), f'vector_buffer.standardized_rew_adv is {vector_buffer.standardized_rew_adv}'
+    assert vector_buffer.buffers is not [], f'vector_buffer.buffers is {vector_buffer.buffers}'
+
+    # checking the store function
+    for _ in range(size):
+        obs = torch.rand((num_envs, obs_dim), dtype=torch.float32, device=device)
+        act = torch.rand((num_envs, act_dim), dtype=torch.float32, device=device)
+        rew = torch.rand((num_envs, 1), dtype=torch.float32, device=device)
+        cost = torch.rand((num_envs, 1), dtype=torch.float32, device=device)
+        val = torch.rand((num_envs, 1), dtype=torch.float32, device=device)
+        cost_val = torch.rand((num_envs, 1), dtype=torch.float32, device=device)
+        log_p = torch.rand((num_envs, 1), dtype=torch.float32, device=device)
+
+        vector_buffer.store(obs, act, rew, val, log_p, cost, cost_val)
+        for idx, buffer in enumerate(vector_buffer.buffers):
+            assert torch.allclose(
+                buffer.obs_buf[buffer.ptr - 1], obs[idx]
+            ), f'buffer.obs[idx] is {buffer.obs_buf[buffer.ptr-1]}'
+            assert torch.allclose(
+                buffer.act_buf[buffer.ptr - 1], act[idx]
+            ), f'buffer.act[idx] is {buffer.act_buf[buffer.ptr-1]}'
+            assert torch.allclose(
+                buffer.rew_buf[buffer.ptr - 1], rew[idx]
+            ), f'buffer.rew[idx] is {buffer.rew_buf[buffer.ptr-1]}'
+            assert torch.allclose(
+                buffer.cost_buf[buffer.ptr - 1], cost[idx]
+            ), f'buffer.cost[idx] is {buffer.cost_buf[buffer.ptr-1]}'
+            assert torch.allclose(
+                buffer.val_buf[buffer.ptr - 1], val[idx]
+            ), f'buffer.val[idx] is {buffer.val_buf[buffer.ptr-1]}'
+            assert torch.allclose(
+                buffer.cost_val_buf[buffer.ptr - 1], cost_val[idx]
+            ), f'buffer.cost_val[idx] is {buffer.cost_val_buf[buffer.ptr-1]}'
+            assert torch.allclose(
+                buffer.logp_buf[buffer.ptr - 1], log_p[idx]
+            ), f'buffer.log_prob[idx] is {buffer.logp_buf[buffer.ptr-1]}'
+
+    # checking the finish_path function
+    for idx, buffer in enumerate(vector_buffer.buffers):
+        last_val = torch.randn(1, device=device)
+        last_cost_val = torch.randn(1, device=device)
+        vector_buffer.finish_path(last_val, last_cost_val, idx)
+        assert (
+            buffer.path_start_idx == buffer.ptr
+        ), f'buffer.path_start_idx is {buffer.path_start_idx}'
+
+    # checking the get function
+    for idx, buffer in enumerate(vector_buffer.buffers):
+        data = vector_buffer.get()
+        assert torch.allclose(
+            data['obs'][idx : size * (idx + 1)], buffer.obs_buf
+        ), f'data[obs][idx] is {data["obs"][idx]}'
+        assert torch.allclose(
+            data['act'][idx : size * (idx + 1)], buffer.act_buf
+        ), f'data[act][idx] is {data["act"][idx]}'
+        assert torch.allclose(
+            data['log_p'][idx : size * (idx + 1)], buffer.logp_buf
+        ), f'data[log_p][idx] is {data["log_p"][idx]}'
+
+
+@helpers.parametrize(
+    obs_dim=[5],
+    act_dim=[5],
+    size=[100],
+    gamma=[0.9],
+    lam=[0.9],
+    adv_estimation_method=['gae', 'vtrace', 'gae-rtg', 'plain'],
+    lam_c=[0.9],
+    penalty_param=[0.0, 1.0],
+    device=['cpu'],
+)
+def test_buffer(
+    obs_dim: int,
+    act_dim: int,
+    size: int,
+    gamma: float,
+    lam: float,
+    adv_estimation_method: str,
+    lam_c: float,
+    penalty_param: float,
+    device: str,
+) -> None:
+    """Test buffer."""
+    # initialize buffer
+    buffer = Buffer(
+        obs_dim=obs_dim,
+        act_dim=act_dim,
+        size=size,
+        gamma=gamma,
+        lam=lam,
+        adv_estimation_method=adv_estimation_method,
+        lam_c=lam_c,
+        penalty_param=penalty_param,
+        device=device,
+    )
+    # checking the initialized value
+    assert buffer.ptr == 0, f'buffer.ptr is {buffer.ptr}'
+    assert buffer.path_start_idx == 0, f'buffer.path_start_idx is {buffer.path_start_idx}'
+    assert buffer.size == size, f'buffer.size is {buffer.size}'
+    assert buffer.max_size == size, f'buffer.max_size is {buffer.max_size}'
+    assert buffer.gamma == gamma, f'buffer.gamma is {buffer.gamma}'
+    assert buffer.lam == lam, f'buffer.lam is {buffer.lam}'
+    assert buffer.lam_c == lam_c, f'buffer.lam_c is {buffer.lam_c}'
+    assert buffer.penalty_param == penalty_param, f'buffer.penalty_param is {buffer.penalty_param}'
+    assert (
+        buffer.adv_estimation_method == adv_estimation_method
+    ), f'buffer.adv_estimation_method is {buffer.adv_estimation_method}'
+    assert buffer.device == device, f'buffer.device is {buffer.device}'
+    expected_data = {
+        'obs': torch.zeros((size, obs_dim), dtype=torch.float32, device=device),
+        'act': torch.zeros((size, act_dim), dtype=torch.float32, device=device),
+        'log_p': torch.zeros((size), dtype=torch.float32, device=device),
+    }
+    # check the store function
+    for _ in range(size):
+        obs, act, rew, val, logp, cost, cost_val = (
+            torch.randn(obs_dim, device=device),
+            torch.randn(act_dim, device=device),
+            torch.randn(1, device=device),
+            torch.randn(1, device=device),
+            torch.randn(1, device=device),
+            torch.randn(1, device=device),
+            torch.randn(1, device=device),
+        )
+        expected_data['obs'][buffer.ptr] = obs
+        expected_data['act'][buffer.ptr] = act
+        expected_data['log_p'][buffer.ptr] = logp
+        buffer.store(obs, act, rew, val, logp, cost, cost_val)
+        assert torch.allclose(
+            buffer.obs_buf[buffer.ptr - 1], obs
+        ), f'buffer.obs_buf[buffer.ptr-1] is {buffer.obs_buf[buffer.ptr-1]}'
+        assert torch.allclose(
+            buffer.act_buf[buffer.ptr - 1], act
+        ), f'buffer.act_buf[buffer.ptr-1] is {buffer.act_buf[buffer.ptr-1]}'
+        assert torch.allclose(
+            buffer.rew_buf[buffer.ptr - 1], rew
+        ), f'buffer.rew_buf[buffer.ptr-1] is {buffer.rew_buf[buffer.ptr-1]}'
+        assert torch.allclose(
+            buffer.val_buf[buffer.ptr - 1], val
+        ), f'buffer.val_buf[buffer.ptr-1] is {buffer.val_buf[buffer.ptr-1]}'
+        assert torch.allclose(
+            buffer.logp_buf[buffer.ptr - 1], logp
+        ), f'buffer.logp_buf[buffer.ptr-1] is {buffer.logp_buf[buffer.ptr-1]}'
+        assert torch.allclose(
+            buffer.cost_buf[buffer.ptr - 1], cost
+        ), f'buffer.cost_buf[buffer.ptr-1] is {buffer.cost_buf[buffer.ptr-1]}'
+        assert torch.allclose(
+            buffer.cost_val_buf[buffer.ptr - 1], cost_val
+        ), f'buffer.cost_val_buf[buffer.ptr-1] is {buffer.cost_val_buf[buffer.ptr-1]}'
+
+    assert buffer.ptr == size, f'buffer.ptr is {buffer.ptr}'
+
+    # check the finish_path function
+    last_val = torch.randn(1, device=device)
+    last_cost_val = torch.randn(1, device=device)
+    buffer.finish_path(last_val=last_val, last_cost_val=last_cost_val)
+    assert buffer.path_start_idx == buffer.ptr, f'buffer.path_start_idx is {buffer.path_start_idx}'
+
+    # check the get function
+    data = buffer.get()
+    assert torch.allclose(data['obs'], expected_data['obs'])
+    assert torch.allclose(data['act'], expected_data['act'])
+    assert torch.allclose(data['log_p'], expected_data['log_p'])
+    assert buffer.ptr == 0, f'buffer.ptr is {buffer.ptr}'
+    assert buffer.path_start_idx == 0, f'buffer.path_start_idx is {buffer.path_start_idx}'
--- tests/test_model.py
+++ tests/test_model.py
@@ -14,23 +14,29 @@
 # ==============================================================================
 """Test models"""
 
+from dataclasses import dataclass
+from typing import Optional
+
 import numpy as np
 import torch
+import torch.nn as nn
 from gymnasium.spaces import Box, Discrete
-from torch.distributions import Categorical
 
 import helpers
 from omnisafe.models import ActorBuilder, CriticBuilder
 from omnisafe.models.actor_critic import ActorCritic
+from omnisafe.models.actor_q_critic import ActorQCritic
 from omnisafe.utils.config_utils import dict2namedtuple
+from omnisafe.utils.model_utils import Activation, InitFunction
 
 
 @helpers.parametrize(
-    obs_dim=[1, 10, 100],
-    act_dim=[1, 5, 10],
+    obs_dim=[10],
+    act_dim=[5],
     shared=[None],
-    hidden_sizes=[64, 128, 256],
-    activation=['tanh', 'softplus', 'sigmoid', 'identity', 'relu'],
+    hidden_sizes=[64],
+    activation=['tanh', 'relu'],
+    use_obs_encoder=[True, False],
 )
 def test_critic(
     obs_dim: int,
@@ -38,8 +44,9 @@ def test_critic(
     shared,
     hidden_sizes: int,
     activation: str,
+    use_obs_encoder: bool,
 ) -> None:
-    """Test critic"""
+    """Test critic."""
     builder = CriticBuilder(
         obs_dim=obs_dim,
         act_dim=act_dim,
@@ -49,7 +56,7 @@ def test_critic(
     )
     obs = torch.randn(obs_dim, dtype=torch.float32)
     act = torch.randn(act_dim, dtype=torch.float32)
-    q_critic = builder.build_critic(critic_type='q')
+    q_critic = builder.build_critic(critic_type='q', use_obs_encoder=use_obs_encoder)
     v_critic = builder.build_critic(critic_type='v')
     out1 = q_critic(obs, act)[0]
     out2 = v_critic(obs)
@@ -58,69 +65,32 @@ def test_critic(
 
 
 @helpers.parametrize(
-    obs_dim=[1, 10, 100],
-    act_dim=[1, 5, 10],
-    hidden_sizes=[64, 128, 256],
-    activation=['tanh', 'softplus', 'sigmoid', 'identity', 'relu'],
+    actor_type=['gaussian', 'gaussian_stdnet'],
+    obs_dim=[10],
+    act_dim=[5],
+    hidden_sizes=[64],
+    activation=['tanh'],
+    output_activation=['tanh'],
     weight_initialization_mode=['kaiming_uniform'],
     shared=[None],
-)
-def test_categorical_actor(
-    obs_dim: int,
-    act_dim: int,
-    hidden_sizes: int,
-    activation: str,
-    weight_initialization_mode: str,
-    shared,
-) -> None:
-    """Test the MLP Categorical Actor class."""
-    builder = ActorBuilder(
-        obs_dim=obs_dim,
-        act_dim=act_dim,
-        hidden_sizes=[hidden_sizes, hidden_sizes],
-        activation=activation,
-        weight_initialization_mode=weight_initialization_mode,
-        shared=shared,
-    )
-    actor = builder.build_actor('categorical')
-
-    obs = torch.randn(obs_dim, dtype=torch.float32)
-    dist = actor(obs)
-    assert isinstance(dist, Categorical), 'Actor output is not a Categorical distribution'
-
-    act = dist.sample()
-    dist, logp = actor(obs, act)
-    assert isinstance(dist, Categorical), 'Actor output is not a Categorical distribution'
-    assert logp.shape == torch.Size([]), f'Actor logp output shape is {logp.shape}'
-
-    act = actor.predict(obs)
-    assert act.shape == torch.Size([]), f'Actor predict output shape is {act.shape}'
-
-    act = actor.predict(obs, deterministic=True)
-    assert act.shape == torch.Size([]), f'Actor predict output shape is {act.shape}'
-
-    act, logp = actor.predict(obs, deterministic=True, need_log_prob=True)
-    assert act.shape == torch.Size([]), f'Actor predict output shape is {act.shape}'
-    assert logp.shape == torch.Size([]), f'Actor logp output shape is {logp.shape}'
-
-
-@helpers.parametrize(
-    obs_dim=[1, 10, 100],
-    act_dim=[1, 5, 10],
-    hidden_sizes=[64, 128, 256],
-    activation=['tanh', 'softplus', 'sigmoid', 'identity', 'relu'],
-    weight_initialization_mode=['kaiming_uniform'],
-    shared=[None],
-    actor_type=['gaussian_annealing', 'gaussian_learning', 'gaussian_stdnet'],
+    std_learning=[True],
+    std_init=[1.0],
+    scale_action=[True],
+    clip_action=[True],
 )
 def test_gaussian_actor(
+    actor_type: str,
     obs_dim: int,
     act_dim: int,
-    hidden_sizes: int,
-    activation: str,
-    weight_initialization_mode: str,
-    shared,
-    actor_type: str,
+    hidden_sizes: list,
+    activation: Activation,
+    weight_initialization_mode: InitFunction,
+    shared: nn.Module,
+    scale_action: bool,
+    clip_action: bool,
+    output_activation: Optional[Activation],
+    std_learning: bool,
+    std_init: float,
 ) -> None:
     """Test the MLP Gaussian Actor class."""
     builder = ActorBuilder(
@@ -130,6 +100,11 @@ def test_gaussian_actor(
         activation=activation,
         weight_initialization_mode=weight_initialization_mode,
         shared=shared,
+        scale_action=scale_action,
+        clip_action=clip_action,
+        output_activation=output_activation,
+        std_learning=std_learning,
+        std_init=std_init,
     )
     kwargs = {
         'act_min': torch.full((act_dim,), -1.0),
@@ -138,43 +113,50 @@ def test_gaussian_actor(
 
     actor = builder.build_actor(actor_type=actor_type, **kwargs)
 
-    obs = torch.randn(obs_dim, dtype=torch.float32)
+    obs = torch.randn((1, obs_dim), dtype=torch.float32)
     dist = actor(obs)
     assert isinstance(dist, torch.distributions.Normal), 'Actor output is not a Normal distribution'
 
-    act = dist.sample()
-    dist, logp = actor(obs, act)
-    assert isinstance(dist, torch.distributions.Normal), 'Actor output is not a Normal distribution'
-    assert logp.shape == torch.Size([]), f'Actor logp output shape is {logp.shape}'
+    raw_act, act = actor.predict(obs)
+    assert act.shape == torch.Size([1, act_dim]), f'Actor predict output shape is {act.shape}'
+    assert raw_act.shape == torch.Size(
+        [1, act_dim]
+    ), f'Actor predict output shape is {raw_act.shape}'
 
-    act = actor.predict(obs)
-    assert act.shape == torch.Size([act_dim]), f'Actor predict output shape is {act.shape}'
+    raw_act, act = actor.predict(obs, deterministic=True)
+    assert act.shape == torch.Size([1, act_dim]), f'Actor predict output shape is {act.shape}'
+    assert raw_act.shape == torch.Size(
+        [1, act_dim]
+    ), f'Actor predict output shape is {raw_act.shape}'
+    raw_act, act, logp = actor.predict(obs, deterministic=True, need_log_prob=True)
 
-    act = actor.predict(obs, deterministic=True)
-    assert act.shape == torch.Size([act_dim]), f'Actor predict output shape is {act.shape}'
-    act, logp = actor.predict(obs, deterministic=True, need_log_prob=True)
-    assert act.shape == torch.Size([act_dim]), f'Actor predict output shape is {act.shape}'
-    assert logp.shape == torch.Size([]), f'Actor logp output shape is {logp.shape}'
+    assert raw_act.shape == torch.Size(
+        [1, act_dim]
+    ), f'Actor predict output shape is {raw_act.shape}'
+    assert act.shape == torch.Size([1, act_dim]), f'Actor predict output shape is {act.shape}'
+    assert logp.shape == torch.Size([1]), f'Actor logp output shape is {logp.shape}'
 
 
 @helpers.parametrize(
-    obs_dim=[1, 10, 100],
-    act_dim=[1, 5, 10],
+    obs_dim=[10],
+    act_dim=[5],
     space_type=[Box, Discrete],
-    standardized_obs=[True, False],
-    scale_rewards=[True, False],
-    shared_weights=[False],  # shared weights not implemented yet in discrete case.
+    shared_weights=[False, True],  # shared weights not implemented yet in discrete case.
     hidden_sizes=[64],
-    activation=['relu'],
-    weight_initialization_mode=['kaiming_uniform'],
-    actor_type=['gaussian_annealing', 'gaussian_learning', 'gaussian_stdnet'],
+    activation=['tanh'],
+    weight_initialization_mode=[
+        'kaiming_uniform',
+        'xavier_normal',
+        'glorot',
+        'xavier_uniform',
+        'orthogonal',
+    ],
+    actor_type=['gaussian', 'gaussian_stdnet'],
 )
 def test_actor_critic(
     obs_dim: int,
     act_dim: int,
     space_type,
-    standardized_obs: bool,
-    scale_rewards: bool,
     shared_weights: bool,
     hidden_sizes: int,
     activation: str,
@@ -187,7 +169,6 @@ def test_actor_critic(
         'pi': {
             'hidden_sizes': [hidden_sizes, hidden_sizes],
             'activation': activation,
-            'actor_type': actor_type,
         },
         'val': {
             'hidden_sizes': [hidden_sizes, hidden_sizes],
@@ -198,6 +179,7 @@ def test_actor_critic(
 
     model_cfgs = dict2namedtuple(
         {
+            'actor_type': actor_type,
             'ac_kwargs': ac_kwargs,
             'weight_initialization_mode': weight_initialization_mode,
             'shared_weights': shared_weights,
@@ -212,32 +194,123 @@ def test_actor_critic(
     actor_critic = ActorCritic(
         observation_space=observation_space,
         action_space=action_space,
-        standardized_obs=standardized_obs,
-        scale_rewards=scale_rewards,
         model_cfgs=model_cfgs,
     )
 
-    obs = torch.randn(obs_dim, dtype=torch.float32)
+    obs = torch.randn((1, obs_dim), dtype=torch.float32)
 
-    act, val, logpro = actor_critic(obs)
+    raw_act, act, val, logpro = actor_critic(obs)
     assert (
-        isinstance(act, np.ndarray)
-        and isinstance(val, np.ndarray)
-        and isinstance(logpro, np.ndarray)
+        isinstance(raw_act, torch.Tensor)
+        and isinstance(act, torch.Tensor)
+        and isinstance(val, torch.Tensor)
+        and isinstance(logpro, torch.Tensor)
     ), 'Failed!'
 
-    act, val, logpro = actor_critic.step(obs)
+    raw_act, act, val, logpro = actor_critic.step(obs)
     assert (
-        isinstance(act, np.ndarray)
-        and isinstance(val, np.ndarray)
-        and isinstance(logpro, np.ndarray)
+        isinstance(raw_act, torch.Tensor)
+        and isinstance(act, torch.Tensor)
+        and isinstance(val, torch.Tensor)
+        and isinstance(logpro, torch.Tensor)
     ), 'Failed!'
 
-    act, val, logpro = actor_critic.step(obs, deterministic=True)
+    raw_act, act, val, logpro = actor_critic.step(obs, deterministic=True)
     assert (
-        isinstance(act, np.ndarray)
-        and isinstance(val, np.ndarray)
-        and isinstance(logpro, np.ndarray)
+        isinstance(raw_act, torch.Tensor)
+        and isinstance(act, torch.Tensor)
+        and isinstance(val, torch.Tensor)
+        and isinstance(logpro, torch.Tensor)
     ), 'Failed!'
 
-    # TODO: Test anneal_exploration method.
+    actor_critic.anneal_exploration(0.5)
+
+
+@helpers.parametrize(
+    obs_dim=[10],
+    act_dim=[5],
+    space_type=[Box, Discrete],
+    shared_weights=[False],  # shared weights not implemented yet in discrete case.
+    hidden_sizes=[64],
+    activation=['tanh'],
+    weight_initialization_mode=[
+        'kaiming_uniform',
+        'xavier_normal',
+        'glorot',
+        'xavier_uniform',
+        'orthogonal',
+    ],
+    actor_type=['gaussian', 'gaussian_stdnet'],
+)
+def test_actor_q_critic(
+    obs_dim: int,
+    act_dim: int,
+    space_type,
+    shared_weights: bool,
+    hidden_sizes: int,
+    activation: str,
+    weight_initialization_mode: str,
+    actor_type: str,
+) -> None:
+    """Test the Actor Critic class."""
+
+    ac_kwargs = {
+        'pi': {
+            'hidden_sizes': [hidden_sizes, hidden_sizes],
+            'activation': activation,
+        },
+        'val': {
+            'hidden_sizes': [hidden_sizes, hidden_sizes],
+            'activation': activation,
+            'num_critics': 1,
+        },
+    }
+    observation_space = Box(low=-1, high=1, shape=(obs_dim,))
+
+    model_cfgs = dict2namedtuple(
+        {
+            'actor_type': actor_type,
+            'ac_kwargs': ac_kwargs,
+            'weight_initialization_mode': weight_initialization_mode,
+            'shared_weights': shared_weights,
+        }
+    )
+
+    if space_type == Discrete:
+        action_space = space_type(act_dim)
+    else:
+        action_space = space_type(low=-1, high=1, shape=(act_dim,))
+
+    actor_critic = ActorQCritic(
+        observation_space=observation_space,
+        action_space=action_space,
+        model_cfgs=model_cfgs,
+    )
+
+    obs = torch.randn((1, obs_dim), dtype=torch.float32)
+
+    raw_act, act, val, logpro = actor_critic(obs)
+    assert (
+        isinstance(raw_act, torch.Tensor)
+        and isinstance(act, torch.Tensor)
+        and isinstance(val, torch.Tensor)
+        and isinstance(logpro, torch.Tensor)
+    ), 'Failed!'
+
+    raw_act, act, val, logpro = actor_critic.step(obs)
+    assert (
+        isinstance(raw_act, torch.Tensor)
+        and isinstance(act, torch.Tensor)
+        and isinstance(val, torch.Tensor)
+        and isinstance(logpro, torch.Tensor)
+    ), 'Failed!'
+
+    raw_act, act, val, logpro = actor_critic.step(obs, deterministic=True)
+    assert (
+        isinstance(raw_act, torch.Tensor)
+        and isinstance(act, torch.Tensor)
+        and isinstance(val, torch.Tensor)
+        and isinstance(logpro, torch.Tensor)
+    ), 'Failed!'
+
+    actor_critic.anneal_exploration(0.5)
