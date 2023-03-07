96826b5174fb1cd8d5beae3e981a09b0e72bd930
Jiayi Zhou <108712610+Gaiejj@users.noreply.github.com>
2023-03-07 10:08:18 +0800

    fix(ppo): fix entropy loss (#135)

--- omnisafe/algorithms/on_policy/base/ppo.py
+++ omnisafe/algorithms/on_policy/base/ppo.py
@@ -61,7 +61,7 @@ class PPO(PolicyGradient):
             ratio, 1 - self._cfgs.algo_cfgs.clip, 1 + self._cfgs.algo_cfgs.clip
         )
         loss = -torch.min(ratio * adv, ratio_cliped * adv).mean()
-        loss += self._cfgs.algo_cfgs.entropy_coef * distribution.entropy().mean()
+        loss -= self._cfgs.algo_cfgs.entropy_coef * distribution.entropy().mean()
         # useful extra info
         entropy = distribution.entropy().mean().item()
         info = {'entropy': entropy, 'ratio': ratio.mean().item(), 'std': std}
