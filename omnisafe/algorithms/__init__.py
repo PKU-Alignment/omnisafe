f24e80958f4919c8f147c0359538cf605d1cd127
Gaiejj <524339208@qq.com>
2023-03-13 22:11:51 +0800

    docs: update note

--- omnisafe/algorithms/__init__.py
+++ omnisafe/algorithms/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 OmniSafe Team. All Rights Reserved.
+# Copyright 2023 OmniSafe Team. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -47,7 +47,6 @@ from omnisafe.algorithms.on_policy import (
 
 
 ALGORITHMS = {
-    # 'off-policy': tuple(off_policy.__all__),
     'on-policy': tuple(on_policy.__all__),
     'off-policy': tuple(off_policy.__all__),
     # 'model-based': tuple(model_based.__all__),
