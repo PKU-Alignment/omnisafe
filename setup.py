f6b5aa4e97fcdaa6b4ed7b7293e92562dae77476
muchvo <118189931+muchvo@users.noreply.github.com>
2023-03-28 14:42:21 +0800

    chore: add MANIFEST.in (#182)
    Co-authored-by: muchvo <borongzh@gmail.com>

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import pathlib
 import re
 import sys
 
+import setuptools
 from setuptools import setup
 
 
@@ -36,7 +37,15 @@ try:
     setup(
         name='omnisafe',
         version=version.__version__,
+        author='OmniSafe Team',
+        author_email='jiamg.ji@gmail.com',
+        description='OmniSafe is an infrastructural framework for accelerating SafeRL research.',
+        url='https://github.com/PKU-MARL/omnisafe',
         entry_points={'console_scripts': ['omnisafe=omnisafe.utils.command_app:app']},
+        packages=setuptools.find_namespace_packages(
+            include=['omnisafe', 'omnisafe.*'],
+        ),
+        include_package_data=True,
     )
 finally:
     if VERSION_CONTENT is not None:
