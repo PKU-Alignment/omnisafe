1c26a87bce6adfce3bb311c07a9518842de02811
Gaiejj <524339208@qq.com>
2023-04-21 11:45:24 +0800

    chore: clean the code

--- examples/benchmarks/run_experiment_grid.py
+++ examples/benchmarks/run_experiment_grid.py
@@ -29,16 +29,14 @@ def train(
     exp_id: str,
     algo: str,
     env_id: str,
-    custom_cfgs: NamedTuple,
-) -> Tuple[float, float, float]:
+    custom_cfgs: dict,
+) -> tuple:
     """Train a policy from exp-x config with OmniSafe.
-
     Args:
         exp_id (str): Experiment ID.
         algo (str): Algorithm to train.
         env_id (str): The name of test environment.
-        custom_cfgs (NamedTuple): Custom configurations.
-        num_threads (int, optional): Number of threads. Defaults to 6.
+        custom_cfgs (dict): Custom configurations.
     """
     terminal_log_name = 'terminal.log'
     error_log_name = 'error.log'
@@ -68,7 +66,7 @@ def train(
 
 
 if __name__ == '__main__':
-    eg = ExperimentGrid(exp_name='Simmer_4_18')
+    eg = ExperimentGrid(exp_name='Safety_Gymnasium_Goal')
 
     # Set the algorithms.
     base_policy = ['PolicyGradient', 'NaturalPG', 'TRPO', 'PPO']
@@ -97,19 +95,22 @@ if __name__ == '__main__':
         warnings.warn('The GPU ID is not available, use CPU instead.', stacklevel=1)
         gpu_id = None
 
-    eg.add('algo', ['PPOSimmerPID', 'TRPOSimmerPID'])
+    eg.add('algo', base_policy + naive_lagrange_policy + first_order_policy + second_order_policy)
     eg.add('logger_cfgs:use_wandb', [False])
-    eg.add('train_cfgs:vector_env_nums', [10])
-    eg.add('train_cfgs:torch_threads', [6])
+    eg.add('train_cfgs:vector_env_nums', [4])
+    eg.add('train_cfgs:torch_threads', [1])
     eg.add('algo_cfgs:steps_per_epoch', [20000])
     eg.add('train_cfgs:total_steps', [10000000])
-    eg.add('seed', [0, 5, 10])
+    eg.add('seed', [0])
     # total experiment num must can be divided by num_pool
     # meanwhile, users should decide this value according to their machine
-    eg.run(train, num_pool=18, gpu_id=gpu_id)
+    eg.run(train, num_pool=12, gpu_id=gpu_id)
 
     # just fill in the name of the parameter of which value you want to compare.
     # then you can specify the value of the parameter you want to compare,
     # or you can just specify how many values you want to compare in single graph at most,
     # and the function will automatically generate all possible combinations of the graph.
     # but the two mode can not be used at the same time.
+    eg.analyze(parameter='env_id', values=None, compare_num=6, cost_limit=25)
+    eg.render(num_episodes=1, render_mode='rgb_array', width=256, height=256)
+    eg.evaluate(num_episodes=1)
