7a93804068a329f39ff343800fff0ab8b58ecf96
Ruiyang Sun <rockmagma02@gmail.com>
2023-04-23 01:40:58 +0800

    Update omnisafe/algorithms/offline/c_crr.py
    Co-authored-by: muchvo <118189931+muchvo@users.noreply.github.com>

--- omnisafe/algorithms/offline/c_crr.py
+++ omnisafe/algorithms/offline/c_crr.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implementation of C_CRR"""
+"""Implementation of C_CRR."""
 
 from copy import deepcopy
 from typing import Tuple
