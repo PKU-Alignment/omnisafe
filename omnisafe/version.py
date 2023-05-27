acf201a3022b66e27d1ed6b9c3657190b84a1768
zmsn-2077 <jiamg.ji@gmail.com>
2023-05-27 21:27:14 +0800

    ver: bump version to 0.5.0

--- omnisafe/version.py
+++ omnisafe/version.py
@@ -14,7 +14,7 @@
 # ==============================================================================
 """OmniSafe: A comprehensive and reliable benchmark for safe reinforcement learning."""
 
-__version__ = '0.3.0'
+__version__ = '0.5.0'
 __license__ = 'Apache License, Version 2.0'
 __author__ = 'OmniSafe Contributors'
 __release__ = False
