7f3fea609e767f9c9c07b4ba13adae7eb7a81032
Gaiejj <524339208@qq.com>
2023-06-24 14:08:45 +0800

    style: fix mypy

--- omnisafe/envs/safety_gymnasium_env.py
+++ omnisafe/envs/safety_gymnasium_env.py
@@ -52,7 +52,7 @@ class SafetyGymnasiumEnv(CMDP):
     need_auto_reset_wrapper: bool = False
     need_time_limit_wrapper: bool = False
 
-    _support_envs: ClassVar = [
+    _support_envs: ClassVar[list[str]] = [
         'SafetyPointGoal0-v0',
         'SafetyPointGoal1-v0',
         'SafetyPointGoal2-v0',
